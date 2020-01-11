aab409fb42e4e59b11424d11ed902ecbb9d5d860
alex-gagnon <56893298+alex-gagnon-firebrand@users.noreply.github.com>
2020-01-10 20:01:56 -0500

    created count for unchecked items

--- app/src/components/Todo.js
+++ app/src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Todo = props => (
     <li>
         <span>{props.todo.text}</span>
-        <input type="checkbox" checked={props.todo.checked} />
+        <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle}/>
         <button onClick={props.onDelete}>delete</button>
     </li>
 )
